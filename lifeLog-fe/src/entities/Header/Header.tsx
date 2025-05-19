b8ef38334df4641242f5ce90a74c5f8a33f015ec
skozenkov <kozenkovs366@gmail.com>
2025-05-19 17:44:54 +0200

    Styles and Header.tsx

--- lifeLog-fe/src/entities/Header/Header.tsx
+++ lifeLog-fe/src/entities/Header/Header.tsx
@@ -1,8 +1,21 @@
+import classes from './styles.module.css';
+import { Logo } from './Logo';
+import Link from 'next/link';
+import { navigationList } from '@/entities/Header/const';
 
 export const Header = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <header className={classes.wrapper}>
+      <nav className={classes.navigation}>
+        <Logo />
+        <ul className={classes.navList}>
+          {navigationList.map((nav) => (
+            <li key={nav.id}>
+              <Link href={nav.href}>{nav.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
---
+++ lifeLog-fe/src/entities/Header/const.ts
@@ -0,0 +1,5 @@
+export const navigationList = [
+  { id: '0', name: 'Diary', href: '/' },
+  { id: '1', name: 'Plans', href: '/plans' },
+  { id: '2', name: 'Goals', href: '/goals' },
+];
